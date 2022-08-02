c7695e6e2da4dd776d2f8d5051fda94b3a388a30
danielSafonyan <danielsafonyan@gmail.com>
2022-08-02 21:32:28 +0100

    Add playRound  function

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 const OPTIONS = ['rock', 'paper', 'scissors']
 
 function getComputeChoice() {
-    randomNum = Math.floor(Math.random() * OPTIONS.length)
-    return OPTIONS[randomNum]
+    randomNum = Math.floor(Math.random() * OPTIONS.length);
+    return OPTIONS[randomNum];
 }
 
 function getUserChoice() {
     while(true) {
         userChoice = prompt("Chose Rock, Paper or Scissors", "scissors");
-        userChoice = userChoice.toLowerCase()
+        userChoice = userChoice.toLowerCase();
         if (OPTIONS.includes(userChoice)) {
-            return userChoice
+            return userChoice;
         } else {
-            alert("Check your spelling. Available options: Rock, Paper, Scissors")
+            alert("Check your spelling. Available options: Rock, Paper, Scissors");
         }
     }
 }
 
-for (let i = 0; i <= 5; i++) {
-    console.log(getUserChoice())
+function playRound() {
+    computerChoice = getComputeChoice();
+    userChoice = getUserChoice()
+
+    if (computerChoice == userChoice) {
+        alert(`Computer has chosen ${computerChoice}. You have chosen ${userChoice}. It's a draw.`)
+    } else if (computerChoice == 'paper' && userChoice == 'rock') {
+        alert(`Computer has chosen ${computerChoice}. You have chosen ${userChoice}. Computer wins.`)
+    } else if (computerChoice == 'scissors' && userChoice == 'paper') {
+        alert(`Computer has chosen ${computerChoice}. You have chosen ${userChoice}. Computer wins.`)
+    } else if (computerChoice == 'rock' && userChoice == 'scissors') {
+        alert(`Computer has chosen ${computerChoice}. You have chosen ${userChoice}. Computer wins.`)
+    } else {
+        alert(`Computer has chosen ${computerChoice}. You have chosen ${userChoice}. You win.`)
+    }
+}
+
+for (let i = 0; i < 10; i++) {
+    playRound()
 }
